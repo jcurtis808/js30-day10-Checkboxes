3213ecfca68894bbb3075dd1565a74a62e515532
Jeremy Curtis <jeremy.curtis808@gmail.com>
2020-05-20 13:07:45 -0500

    adding comments

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let inputs = document.querySelectorAll('input');
+let checkboxes = document.querySelectorAll('input'); //selecting all checkboxes (input elements)
 
+
+//creating for loop to get the last index of the checked boxes. This loop starts from the highest index and returns first matching index (last checked).
 const lastIndex = (arr) => {
-    let index = -1;
     for (let i = arr.length -1; i >= 0; i--) {
         if (arr[i].checked === true) return i;
     }
@@ -9,16 +10,16 @@ const lastIndex = (arr) => {
 }
 
 function handleCheck(e) {
-    if(!e.shiftKey) return;
-    let inputs2 = Array.from(inputs);
-    let first = inputs2.findIndex(input => input.checked);
-    let last = lastIndex(inputs2);
-    inputs2.filter((input, i) => {
+    if(!e.shiftKey) return;  //found that click events have a property directly related to the shiftkey
+    let inputs = Array.from(checkboxes);  //creating array from nodeList for method use
+    let first = inputs.findIndex(input => input.checked);  //index of first checked box
+    let last = lastIndex(inputs);  //index of last checked box
+    inputs.filter((input, i) => {  //filtering original array with only indices that are between first and last (including first and last) 
         if(i >= first && i <= last)
         return input;
-    }).forEach(input => input.checked = true);
+    }).forEach(input => input.checked = true);  //checking all boxes that meet the filter
 }
 
 
 
-inputs.forEach(input => input.addEventListener('click', handleCheck));
+checkboxes.forEach(input => input.addEventListener('click', handleCheck));
